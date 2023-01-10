e72ec1ad7d01e9940205ef98de126b9ec7d6c356
JustynaMedrala <medrala.justyna341@gmail.com>
2023-01-10 17:02:19 +0100

    initial

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from os import listdir, system
 import pathlib
 
 def run_cmd(i):
-    cmd = '/mnt/home/jmedrala/j-pet-framework-examples/build/NewAnalysis/LargeBarrelAnalysis.x -t mcGeant -f ' + i + ' -o ./ -p conf_trb3.xml -u /mnt/home/jmedrala/workspace/userParams_mc.json -i 9 -l /mnt/home/jmedrala/j-pet-framework-examples/CalibrationFiles/9_RUN/detectorSetupRun9.json'
+    cmd = '/mnt/home/jmedrala/j-pet-framework-examples/build/NewAnalysis/LargeBarrelAnalysis.x -t mcGeant -f ' + i + ' -o ../output/ -p conf_trb3.xml -u /mnt/home/jmedrala/workspace/userParams_mc.json -i 9 -l /mnt/home/jmedrala/j-pet-framework-examples/CalibrationFiles/9_RUN/detectorSetupRun9.json'
     system(cmd)
 
 listOfFiles = []
